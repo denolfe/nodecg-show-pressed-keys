c0a39179753169dac0405840106730959de47c93
Elliot DeNolf <denolfe@gmail.com>
2015-04-18 13:47:30 -0600

    Added input box to show recently typed words

--- dashboard/panel.js
+++ dashboard/panel.js
@@ -6,3 +6,8 @@ $('#row1').change(function() {
     console.log($('#row1').is(":checked"));
     nodecg.sendMessage('row1', { value: $('#row1').is(":checked") });     
 });
+
+$('#showInput').change(function() {
+    console.log($('#showInput').is(":checked"));
+    nodecg.sendMessage('showInput', { value: $('#showInput').is(":checked") });
+});
